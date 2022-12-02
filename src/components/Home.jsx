c5f811754b1f8be830bce648ea7a18ebcae4c020
Sam Greaves <sam.greaves@bglgroup.co.uk>
2022-12-02 15:28:34 +0000

    start adding prop header

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import PropertiesHeader from "./PropertiesHeader";
+
+class Home extends Component {
+
+    render() {
+        return (
+        <div>
+            <PropertiesHeader></PropertiesHeader>
+        </div>);
+    }
+}
+
+export default Home;
---
+++ src/components/PropertiesHeader.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class PropertiesHeader extends Component {
+
+    render() {
+        return (
+            <div>
+                <text>test</text>
+            </div>);
+    }
+}
+
+export default PropertiesHeader;
